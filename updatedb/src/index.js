dc7ac6b46b3ec5e054767d808b2c437a413c5b79
Dmitry Mazurok <mazurok@outlook.com>
2017-08-26 12:05:51 +0700

    script to update db is working

--- updatedb/src/index.js
+++ updatedb/src/index.js
@@ -1,29 +1,76 @@
 #!/usr/bin/env node
 
+const mysql = require('mysql');
 const log = require('./log')(module);
-const mongodb = require('mongodb');
-const mongo = mongodb.MongoClient;
 
-const args = parseArgs(process.argv.slice(2));
-dowork(args);
+log.debug("process.env", process.env);
+const config = parseConfig(process.env);
+log.debug("Using config", config);
+dowork(config);
 
-async function dowork({ url }) {
-    log.info("Starting putting data to mongo");
+function dowork(config) {
+    log.info("Starting putting data to database");
 
+    const connection = mysql.createConnection({
+        host: config.mysql.host,
+        user: config.mysql.user,
+        password: config.mysql.password
+    });
+    connection.connect(function (err) {
+        if (err) handleError(err, "Error connecting to database");
+        log.info("Connected to database as id", connection.threadId);
 
-    const db = await mongo.connect(url);
-    log.info("Connected to mongo");
+        connection.query('CREATE DATABASE IF NOT EXISTS my_db', function (err) {
+            if (err) handleError(err, "Error creating database");
 
-    try {
-        log.debug("hello");
-    } finally {
-        log.info("Disconnecting from mongo");
-        db.close();
-    }
+            connection.changeUser({ database: config.mysql.database }, function (err) {
+                if (err) handleError(err, "Couldn't change database for the connection");
+
+                connection.query('CREATE TABLE IF NOT EXISTS mytable (id INT, name CHAR(100), title CHAR(50))', function (err) {
+                    if (err) handleError(err, "Error creating table");
+
+                    const entry = generateEntry();
+
+                    connection.query('INSERT INTO mytable SET ?', entry, function (err, res, fields) {
+                        if (err) handleError(err, "Error inserting data", entry);
+
+                        log.debug("res", res);
+                        log.debug("fields", fields);
+                        log.info("Success! Inserted entry", entry);
+
+                        log.info("Disconnecting from database");
+                        connection.end(function (err) {
+                            if (err) handleError(err, "Error disconnecting from database");
+                            log.info("Disconnected from database");
+                        });
+                    });
+                });
+            });
+        });
+
+    });
 }
 
-function parseArgs(args) {
+function parseConfig(envArgs) {
     return {
-        url: args[0] || "mongodb://mongo:27017/poccron"
+        mysql: {
+            user: envArgs.MYSQL_USER,
+            password: envArgs.MYSQL_PASSWORD,
+            host: envArgs.MYSQL_HOST,
+            database: envArgs.MYSQL_DB
+        }
+    };
+}
+
+function handleError(error, msg) {
+    msg = msg || "Error happened";
+    log.error(msg, error);
+    throw error;
+}
+
+function generateEntry() {
+    return {
+        name: "myname",
+        title: "mytitle"
     };
 }
